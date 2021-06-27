c17d11b7b59ab9d0934218d29aafbbce1779961b
Jorge Carrillo <jorgecardi00@gmail.com>
2021-06-26 19:26:45 -0500

    Initial commit

---
+++ app/src/main/java/co/edu/unipiloto/MainActivity.java
@@ -0,0 +1,19 @@
+package co.edu.unipiloto;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onSendMessage(View view){
+
+    }
+}
